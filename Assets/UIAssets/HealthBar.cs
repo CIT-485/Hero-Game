3d683028c7235e3795373980d5e11a6d63b1f471
Baldur-Skyguard <89748270+Baldur-Skyguard@users.noreply.github.com>
2021-11-23 14:47:27 -0600

    1
    1

---
+++ Assets/UIAssets/HealthBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    void Awake()
+    {
+        if (GetComponent<Slider>() != null)
+            slider = GetComponent<Slider>();
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
