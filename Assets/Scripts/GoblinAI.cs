33bd70c2b51da3dcb66ba0ca47bc13a6ddaffc54
Chinnow <knightjc246@gmail.com>
2021-11-23 18:06:05 -0600

    GoblinAI

--- Assets/Scripts/GoblinAI.cs
+++ Assets/Scripts/GoblinAI.cs
@@ -29,6 +29,7 @@ public class GoblinAI : Enemy
     public float jumpForce = 2.5f;
     public int waypointIndex = 0;
     public bool facingRight;
+    public bool doOnce;
 
     public List<Waypoint> waypoints = new List<Waypoint>();
 
@@ -47,9 +48,11 @@ public class GoblinAI : Enemy
         tree.blackboard.delegates.GetValue("Patrol") = Patrol;
         tree.blackboard.delegates.GetValue("Aggro") = Aggro;
         tree.blackboard.delegates.GetValue("Attack") = Attack;
+        tree.blackboard.delegates.GetValue("Dead") = Dead;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         render = GetComponent<SpriteRenderer>();
+        am = GetComponent<AttackManager>();
     }
 
     // Update is called once per frame
@@ -58,7 +61,7 @@ public class GoblinAI : Enemy
         tree.blackboard.integers.GetValue("currentHP") = health.currentHealth;
         tree.blackboard.floats.GetValue("distance") = Mathf.Abs(player.transform.position.x - transform.position.x);
         tree.Update();
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -67,6 +70,21 @@ public class GoblinAI : Enemy
         {
             health.TakeDamage(player.GetComponent<AttackManager>().currentAttack.attackDamage);
         }
+    }
+
+    Node.State Dead()
+    {
+        healtBarCanvas.SetActive(false);
+        tree.blackboard.booleans.GetValue("IsActive") = false;
+        ReduceVelocity();
+        if (!doOnce)
+        {
+            animator.SetTrigger("Death");
+            doOnce = true;
+        }
+        if (isAbsorbed)
+            return Node.State.SUCCESS;
+        return Node.State.RUNNING;
     }
 
     Node.State Patrol()
@@ -131,24 +149,112 @@ public class GoblinAI : Enemy
 
     Node.State Aggro()
     {
-        if (tree.blackboard.floats.GetValue("distance") > 5)
-        {
+        if (IsDead())
             return Node.State.FAILURE;
-        }
+        // The attack wait time is also built up during the chase.
+        attackWaitTime += Time.deltaTime;
+        tree.blackboard.floats.GetValue("distance") = Mathf.Abs(player.transform.position.x - transform.position.x);
+
+        // if the player is within attacking distance, then it will return success
         if (tree.blackboard.floats.GetValue("distance") < 1)
         {
-            return Node.State.FAILURE;
+            animator.SetInteger("AnimState", 0);
+            return Node.State.SUCCESS;
         }
+
+        // if the player is outside agro range, then it will return failure
+        else if (tree.blackboard.floats.GetValue("distance") > 5f)
+        {
+            animator.SetInteger("AnimState", 0);
+            return Node.State.FAILURE;
+
+        }
+
+        // if the bandit hits a wall that reduces it's horizontal velocity to zero, then it will attempt to jump
+        if (rb.velocity.x == 0)
+            jumpWaitTime += Time.deltaTime;
+        else
+            jumpWaitTime = 0;
+        if (jumpWaitTime > 0.1f)
+            Jump();
+
+        // This will correct the facing direction of the bandit and allows it to move
+        Vector2 directionalForce = Vector2.zero;
+        animator.SetInteger("AnimState", 2);
+        if (transform.position.x < player.transform.position.x)
+        {
+            if (facingRight)
+                ChangeFacingDirection();
+            directionalForce += new Vector2(acceleration, 0);
+        }
+        else
+        {
+            if (!facingRight)
+                ChangeFacingDirection();
+            directionalForce += new Vector2(-acceleration, 0);
+        }
+        if (currentStop != null)
+        {
+            if ((player.transform.position.x > currentStop.transform.position.x && directionalForce.x <= 0) ||
+                (player.transform.position.x < currentStop.transform.position.x && directionalForce.x >= 0))
+                Move(directionalForce);
+            else
+                ReduceVelocity();
+        }
+        else
+            Move(directionalForce);
         return Node.State.RUNNING;
     }
 
     Node.State Attack()
     {
-        if (tree.blackboard.floats.GetValue("distance") > 1)
-        {
+        if (IsDead())
             return Node.State.FAILURE;
+        // Increase wait time by the amount of seconds that has elapsed since the last frame
+        attackWaitTime += Time.deltaTime;
+        tree.blackboard.floats.GetValue("distance") = Mathf.Abs(player.transform.position.x - transform.position.x);
+        Debug.Log(tree.blackboard.floats.GetValue("distance"));
+
+        // If an attack is not in motion
+        if (!isAttacking)
+        {
+            animator.SetInteger("AnimState", 0);
+            // When the player is out of range, then this action will fail
+            if (tree.blackboard.floats.GetValue("distance") > 1)
+                return Node.State.FAILURE;
+
+            // This will correct the facing direction of the bandit
+            if (transform.position.x < player.transform.position.x)
+            {
+                if (facingRight)
+                    ChangeFacingDirection();
+            }
+            else
+            {
+                if (!facingRight)
+                    ChangeFacingDirection();
+            }
         }
 
+        // This is to reduce the bandit's velocity so it's standing still
+        ReduceVelocity();
+
+        // When the attack wait time is over 3 seconds, then it will decide on an attack
+        if (attackWaitTime > 3)
+        {
+            int attackChance = Random.Range(0, 100);
+            float holdTime = Random.Range(0.5f, 0.75f);
+            if (attackChance < 75)
+            {
+                // it will have a 75% chance of performing an attack after which the attack wait time is refreshed back to zero 
+                isAttacking = true;
+                attackWaitTime = 0;
+                StartCoroutine(Attack0(holdTime));
+            }
+            else
+                // if it chooses not to attack, then the attack wait time is refreshed, but at a lower cooldown
+                attackWaitTime = 2f;
+        }
         return Node.State.RUNNING;
     }
 
@@ -180,11 +286,13 @@ public class GoblinAI : Enemy
     {
         if (facingRight)
         {
+            facingRight = false;
             render.flipX = false;
             FixHitboxes(attackHitboxes);
         }
         else
         {
+            facingRight = true;
             render.flipX = true;
             FixHitboxes(attackHitboxes);
         }
@@ -196,4 +304,22 @@ public class GoblinAI : Enemy
             return true;
         return false;
     }
+    IEnumerator Attack0(float time)
+    {
+        am.SetAttack("Attack0");
+        animator.SetTrigger("isAttacking");
+        yield return new WaitForSeconds(time);
+    }
+
+    void AttackEnd()
+    {
+        isAttacking = false;
+        attackHitboxes.SetActive(false);
+    }
+
+    void AttackHitboxStart()
+    {
+        attackHitboxes.SetActive(true);
+    }
+
 }
